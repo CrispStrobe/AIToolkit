8c8504f15b22af698830001357ec68635e05bc87
CrispStrobe <cze@mailbox.org>
2025-12-05 11:08:31 +0000

    add license

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2025 CrispStrobe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 # /var/www/transkript_app/app.py
 import gradio as gr
 import os
